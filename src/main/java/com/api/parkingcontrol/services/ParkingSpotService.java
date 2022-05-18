0a42af6248c1cd0ada17eb2533bc02dcefc79805
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2022-05-18 20:43:13 -0300

    feat(service): parking spaces service layer

---
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,25 @@
+package com.api.parkingcontrol.services;
+
+import com.api.parkingcontrol.models.ParkingSpotModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ParkingSpotService {
+    ParkingSpotModel save(ParkingSpotModel model);
+
+    boolean existsByLicensePlateCar(String licensePlateCar);
+
+    boolean existsByParkingSpotNumber(String parkingSpotNumber);
+
+    boolean existsByApartmentAndBlock(String apartment, String block);
+
+    Page<ParkingSpotModel> findAll(Pageable pageable);
+
+    Optional<ParkingSpotModel> findById(UUID id);
+
+    void delete(ParkingSpotModel parkingSpotModel);
+}
---
+++ src/main/java/com/api/parkingcontrol/services/impl/ParkingSpotServiceImpl.java
@@ -0,0 +1,59 @@
+package com.api.parkingcontrol.services.impl;
+
+import com.api.parkingcontrol.models.ParkingSpotModel;
+import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+import com.api.parkingcontrol.services.ParkingSpotService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class ParkingSpotServiceImpl implements ParkingSpotService {
+    final ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotServiceImpl(ParkingSpotRepository parkingSpotRepository) {
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+    @Override
+    @Transactional
+    public ParkingSpotModel save(ParkingSpotModel model) {
+        return this.parkingSpotRepository.save(model);
+    }
+
+    @Override
+    public boolean existsByLicensePlateCar(String licensePlateCar) {
+        return this.parkingSpotRepository.existsByLicensePlateCar(licensePlateCar);
+    }
+
+    @Override
+    public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
+        return this.parkingSpotRepository.existsByParkingSpotNumber(parkingSpotNumber);
+    }
+
+    @Override
+    public boolean existsByApartmentAndBlock(String apartment, String block) {
+        return this.parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
+    }
+
+    @Override
+    public Page<ParkingSpotModel> findAll(Pageable pageable) {
+        return this.parkingSpotRepository.findAll(pageable);
+    }
+
+    @Override
+    public Optional<ParkingSpotModel> findById(UUID id) {
+        return this.parkingSpotRepository.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public void delete(ParkingSpotModel parkingSpotModel) {
+        this.parkingSpotRepository.delete(parkingSpotModel);
+    }
+}
